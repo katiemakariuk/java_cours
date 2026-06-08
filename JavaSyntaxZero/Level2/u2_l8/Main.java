b8fe8adeb269c9616fd9c5dc8dc55e50a403f2f1
Katie Makariuk <katiemakariuk@gmail.com>
2026-06-08 19:18:10 +0200

    Adding lesson 8 of level 2

---
+++ JavaSyntaxZero/Level2/u2_l8/Main.java
@@ -0,0 +1,15 @@
+package JavaSyntaxZero.Level2.u2_l8;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s1 = scanner.nextLine();
+        String s2 = scanner.nextLine();
+        String s3 = scanner.nextLine();
+        System.out.println(s3);
+        System.out.println(s2.toUpperCase());
+        System.out.println(s1.toLowerCase());
+    }
+}
---
+++ JavaSyntaxZero/Level2/u2_l8/Main2.java
@@ -0,0 +1,13 @@
+package JavaSyntaxZero.Level2.u2_l8;
+
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i1 = scanner.nextInt();
+        int i2 = scanner.nextInt();
+        int i3 = scanner.nextInt();
+        System.out.println((i1 + i2 + i3) / 3);
+    }
+}
