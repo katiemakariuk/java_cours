c3ff9f5525830572942c5036f13b6da43042c954
Katie Makariuk <katiemakariuk@gmail.com>
2026-06-08 19:13:21 +0200

    Adding lesson 6 of level 2

---
+++ JavaSyntaxZero/Level2/u2_l6/Main5.java
@@ -0,0 +1,10 @@
+package JavaSyntaxZero.Level2.u2_l6;
+
+public class Main5 {
+    public static void main(String[] args) {
+        String emptyString = "";
+        System.out.println(emptyString.length());
+        System.out.println("Gomu Gomu no Bazooka!".length());
+        System.out.println((emptyString + 2 + 2 + "22").length());
+    }
+}
---
+++ JavaSyntaxZero/Level2/u2_l6/Main7.java
@@ -0,0 +1,11 @@
+package JavaSyntaxZero.Level2.u2_l6;
+
+public class Main7 {
+    public static void main(String[] args) {
+        String caps = "if I type in caps ";
+        String usa = "сша";
+        System.out.println(usa.toUpperCase());
+        System.out.println("Вінні-Пух".toUpperCase());
+        System.out.println((caps + "they know I mean business").toUpperCase());
+    }
+}
