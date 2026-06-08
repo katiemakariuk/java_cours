670835cd095ca8b2a0b1cf5099247e63b6106640
Katie Makariuk <katiemakariuk@gmail.com>
2026-06-08 19:05:45 +0200

    Adding lesson 6 of level 2

---
+++ JavaSyntaxZero/Level2/u2_l6/Main.java
@@ -0,0 +1,12 @@
+package JavaSyntaxZero.Level2.u2_l6;
+
+public class Main {
+    public static void main(String[] args) {
+        String s1 = "Do";
+        String s2 = "It";
+        String s3 = "Just";
+
+        String result = s1 + s2 + s3;
+        System.out.println(result);
+    }
+}
