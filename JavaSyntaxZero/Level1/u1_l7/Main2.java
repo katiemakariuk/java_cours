6ba0301886f509e4e47eda2cbc075753f313553e
Katie Makariuk <katiemakariuk@gmail.com>
2026-06-08 18:50:03 +0200

    Adding lesson 7 version 2  of level 1

---
+++ JavaSyntaxZero/Level1/u1_l7/Main2.java
@@ -0,0 +1,19 @@
+package JavaSyntaxZero.Level1.u1_l7;
+
+public class Main2 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 2;
+
+//        System.out.print("два");
+        System.out.print(b);
+        System.out.print(" + ");
+//        System.out.print(" мінус ");
+        System.out.print(a);
+//        System.out.print("три");
+        System.out.print(" = ");
+//        System.out.print(" буде ");
+//        System.out.print("п'яти");
+        System.out.print(a + b);
+    }
+}
